e0ee8a4a5543a8cec12602acd7923b7e5af879e4
p.collado <p.collado@alumnos.upm.es>
2022-10-28 23:19:36 +0200

    Viernes 28

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include "..\project\Vec2.h"
+#include "stb_image.h"
 
 #define HEIGHT 720
 #define WIDTH 1280
@@ -22,16 +23,19 @@ template <typename T> std::string stringFromNumber(T val)
 
 int main() 
 {
+
 	string title;
 
 	double x = 0.f;
 	double y = 0.f;
-	double objx = 0.f;
-	double objy = 0.f;
 	double time = 0.f;
-	double angulo = 0.f;
-	double distance = 0.f;
-	double mov = 0.f;
+
+
+	struct sTam {
+		int ancho;
+		int alto;
+		int* Comp;
+	};
 
 
 
@@ -43,9 +47,18 @@ int main()
 	
 	Vec2 Cursor(x, y);
 	Vec2 PosObject;
+	sTam Fire, valla, fondo, filtro;
 
 	GLFWwindow* pWindow = glfwCreateWindow(WIDTH, HEIGHT, "Prueba", nullptr, nullptr);
 	glfwMakeContextCurrent(pWindow);
+
+	Fire.alto = 256 / 2;
+	Fire.ancho = 256 / 2;
+
+	unsigned char* ptrs = stbi_load("sprites\\fire.png",&Fire.ancho,&Fire.alto,Fire.Comp,4);
+
+	stbi_image_free(Fire.Comp);
+
 
 	while (!glfwWindowShouldClose(pWindow))
 	{
@@ -55,27 +68,19 @@ int main()
 		glfwGetCursorPos(pWindow,&x , &y);
 	
 		Cursor = Vec2(x,y);
-		title = "Distancia: " + stringFromNumber(distance) + " -- Angulo: " + stringFromNumber(angulo);
+		
+		//title = "Distancia: " + stringFromNumber(distance) + " -- Angulo: " + stringFromNumber(angulo);
 		glfwSetWindowTitle(pWindow, title.c_str());
-		mov += ANGULO * (time - glfwGetTime());
-		objx = Cursor.GetfX() + 100 * sin(mov) - 25;
-		objy = Cursor.GetfY() + 100 * cos(mov) - 25;
-		PosObject = Vec2(objx, objy);
-		angulo = Cursor.Angle(PosObject);
-		distance = Cursor.Distance(Vec2(WIDTH/2, HEIGHT/2));
 	
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		lgfx_setcolor(1.f, 0.f, 0.f, 0.f);
-		lgfx_drawrect((WIDTH / 2) - 50, (HEIGHT / 2) - 50, 100, 100);
-
-
 		lgfx_setcolor(0.f, 1.f, 0.f, 0.f);
 		lgfx_drawrect(x - 50, y - 50, 100, 100);
 
-		lgfx_setcolor(0.f, 0.f, 1.f, 0.f);
-		lgfx_drawoval(objx, objy, 50, 50);
+		ltex_t * ptr = ltex_alloc(Fire.ancho, Fire.alto,0);
+
+		ltex_setpixels(ptr, ptrs);
 
 		time = glfwGetTime();
 
