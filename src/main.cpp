2f16ca291003b0e195d77bf121029c784a0afc25
p-collado <p.collado@alumnos.upm.es>
2022-12-02 19:54:40 +0100

    Practica 5 Init

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,6 @@ using namespace std;
 float xpos;
 float ypos;
 
-void SeekingMouse(CSprite& _Sprite, float _delta)
-{
-	Vec2 Distance;
-	Vec2 Cursor = *(static_cast<Vec2*>(_Sprite.getUserData()));
-
-	Distance = Cursor - _Sprite.getPosition();
-	if (!(Distance.GetfX() == 0.f && Distance.GetfY() == 0))
-	{
-		Distance = Cursor.Normalvector(Distance);
-		Vec2 NewPos = Distance * 128.f * _delta;
-		NewPos = NewPos + _Sprite.getPosition();
-		_Sprite.setPosition(NewPos);
-	}
-}
-
 int main() 
 {
 	if (!glfwInit())
@@ -44,15 +29,12 @@ int main()
 	lgfx_setup2d(WIDTH, HEIGHT);
 
 	CSprite* abeja = CSprite::loadTexture("sprites\\bee_anim.png");
-	abeja->setFps(8);
-	abeja->setMaxRot(-15);
 	Vec2 Cursor(x, y);
 	Vec2 Distance;
 
-	float angle = 0.f;
 	double time = 0.f;
 	double deltaTime = 0;
-	float resta = 0.f;
+
 
 	while (!glfwWindowShouldClose(pWindow))
 	{
@@ -61,19 +43,16 @@ int main()
 		glfwPollEvents();
 		glfwGetCursorPos(pWindow,&x , &y);
 
-		//Seek mouse and bee Movement
 		Cursor = Vec2(x,y);
-		abeja->setUserData(&Cursor);
-		abeja->setCallback(&SeekingMouse);
 
 		xpos = Cursor.GetfX();
 		ypos = Cursor.GetfY();
 	
 		/* Render here */
 		lgfx_clearcolorbuffer(0.f, 0.f, 0.f);
-		
 		lgfx_setblend(BLEND_ALPHA);
-		abeja->update(deltaTime);
+
+
 		time = glfwGetTime();
 
 		/* Swap front and back buffers */
